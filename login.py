4a2323a24f89ee3c23acd9ff1cf0d666cbc42598
GrapherD <grapherd@gmail.com>
2014-01-02 19:24:42 +0800

    clean code

--- login.py
+++ login.py
@@ -22,9 +22,12 @@ password     = "6019"
 
 headers = {}
 headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0"
+headers['Accept'] = "zh-tw,en-us;q=0.7,en;q=0.3"
+headers['Accept-Encoding'] = "gzip, deflate"
+headers['Referer'] = "http://ep.kuas.edu.tw/EPortfolio/EPDefaultPage.aspx"
+headers['Connection'] = "keep-alive"
 
-
-def login():
+def Login():
 	try:
 		session = requests.session()
 		response = session.get(POST_URL , headers = headers)
@@ -32,34 +35,9 @@ def login():
 		raise e
 
 	payload = collections.OrderedDict()
-	raw_data = ['__EVENTTARGET','','__EVENTARGUMENT','',
-				'__VIEWSTATE', '',
-				'ctl00$LoginForm1$TbLoginId', username,
+	raw_data = ['ctl00$LoginForm1$TbLoginId', username,
 				'ctl00$LoginForm1$TbLoginPW', password,
-				'ctl00$LoginForm1$BtLogin','登入',
-				'ctl00$ContentPlaceHolder1$HFUrlPath','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl00$HF1','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl00$HF2','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl00$HF3','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl00$HFAnnId','7',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl01$HF1','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl01$HF2','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl01$HF3','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl01$HFAnnId','6',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl02$HF1','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl02$HF2','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl02$HF3','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl02$HFAnnId','5',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl03$HF1','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl03$HF2','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl03$HF3','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl03$HFAnnId','4',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl04$HF1','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl04$HF2','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl04$HF3','',
-				'ctl00$ContentPlaceHolder1$DataListPost$ctl04$HFAnnId','3',
-				'ctl00$BlockUIShowFlag','',
-				'ctl00$HFBlockTimeOut','']
+				'ctl00$LoginForm1$BtLogin','登入']
 
 	
 	for x in xrange(0,len(raw_data),2):
@@ -71,13 +49,6 @@ def login():
 			payload['__VIEWSTATE'] = i.attrib['value']
 		if i.attrib['name'] == '__EVENTVALIDATION':
 			payload['__EVENTVALIDATION'] = i.attrib['value']
-			
-		
-	# Setting Headers
-	headers['Accept'] = "zh-tw,en-us;q=0.7,en;q=0.3"
-	headers['Accept-Encoding'] = "gzip, deflate"
-	headers['Referer'] = "http://ep.kuas.edu.tw/EPortfolio/EPDefaultPage.aspx"
-	headers['Connection'] = "keep-alive"
 	
 	
 	# RePost Data
@@ -88,14 +59,14 @@ def login():
 	
 	
 	if result:
-		print u"Login Success! %s" % (result[0].text)
-		print response.request.headers['Cookie']
+		#print u"Login Success! %s" % (result[0].text)
+		#print response.request.headers['Cookie']
 		return 1
 	else:
-		print u"Login Falied!"
+		#print u"Login Falied!"
 		return 0
 
 
 if __name__ == '__main__':
-	login()
+	Login()
 
--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from lxml import etree
 import login
 
 if __name__ == '__main__':
-    login.login()
-    response = requests.get(login.ACTIVE_URL , headers = login.headers)
-    root = etree.HTML(response.text)
-    print u'\n\n讀取活動資料中......'
-
-    for i in root.xpath("//a[starts-with(@id, 'ContentPlaceHolder1_ContentPlaceHolder1_TabContainer1_PopularPanel_gvPopular_LBact_name_')]"):
-        print i.text
-  
+    if login.Login():
+        response = requests.get(login.ACTIVE_URL , headers = login.headers)
+        root = etree.HTML(response.text)
+    
+        for i in root.xpath("//a[starts-with(@id, 'ContentPlaceHolder1_ContentPlaceHolder1_TabContainer1_PopularPanel_gvPopular_LBact_name_')]"):
+            print i.text
+      

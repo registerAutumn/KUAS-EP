b3a4741338eec37eb83fdceef9b8f75660244f85
GrapherD <grapherd@gmail.com>
2014-01-02 18:09:58 +0800

    update

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     login.login()
     response = requests.get(login.ACTIVE_URL , headers = login.headers)
     root = etree.HTML(response.text)
-    print u'\n\n讀取活動資料中...'
-    
+    print u'\n\n讀取活動資料中......'
+
     for i in root.xpath("//a[starts-with(@id, 'ContentPlaceHolder1_ContentPlaceHolder1_TabContainer1_PopularPanel_gvPopular_LBact_name_')]"):
         print i.text
   
